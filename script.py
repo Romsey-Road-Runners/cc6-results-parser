46424c6b27e0a4f3c8fef331a289a4a585c1b83f
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2023-12-11 13:00:56 +0000

    Process team and overall results

--- script.py
+++ script.py
@@ -74,6 +74,50 @@ def parse_csv(file_name, race):
     return sorted(results_list, key=lambda d: d["position"])
 
 
+def parse_team_csv(file_name, race):
+    csvfile = open(file_name, "r")
+    field_names = [
+        "Name",
+        "R1 Total",
+        "R2 Total",
+        "R3 Total",
+        "R4 Total",
+        "R5 Total",
+        "R6 Total",
+        "R7 Total",
+        "Blank",
+        "R1",
+        "R2",
+        "R3",
+        "R4",
+        "R5",
+        "R6",
+        "R7",
+        "Total",
+    ]
+    reader = csv.DictReader(csvfile, field_names)
+    results_list = []
+    for row in reader:
+        if "TEAM" in row["Name"]:
+            continue
+
+        results_list.append(
+            {
+                "name": row["Name"].strip(),
+                "position": int(row[f"R{race}"]) if row[f"R{race}"] != "ORG" else 0,
+                "leaguePosition": int(row["Total"]),
+            }
+        )
+    league_position_counter = 0
+    team_results_list = []
+    for team in sorted(results_list, key=lambda d: d["leaguePosition"]):
+        league_position_counter += 1
+        team["leaguePosition"] = league_position_counter
+        team_results_list.append(team)
+
+    return sorted(team_results_list, key=lambda d: d["position"])
+
+
 sexes = ["Men", "Women"]
 age_cats = ["", "V40", "V50", "V60", "V70"]
 race = sys.argv[1]
@@ -81,6 +125,7 @@ file_prefix = f"Race-{race}-Results"
 file_list = []
 complete_results_dict = {}
 
+# Process results for age cats and overall men and women
 for combination in [(sex, age_cat) for sex in sexes for age_cat in age_cats]:
     maybe_dash = "-" if combination[1] else ""
     file_name = f"{file_prefix}-{combination[0]}{maybe_dash}{combination[1]}.csv"
@@ -89,6 +134,7 @@ for combination in [(sex, age_cat) for sex in sexes for age_cat in age_cats]:
     )
     file_list.append({"file_name": file_name, "dict_key": dict_key})
 
+# Cheekily add overall results file into the mix
 file_list += [{"file_name": file_prefix + "-Overall.csv", "dict_key": "Overall"}]
 
 for file in file_list:
@@ -96,5 +142,17 @@ for file in file_list:
     print("Processing " + file_name)
     complete_results_dict[file["dict_key"]] = parse_csv(file_name, race)
 
+# Process team results
+for team_file in [
+    {"file_name": f"{file_prefix}-{sex}-Team.csv", "dict_key": f"{sex}s Teams"}
+    for sex in ["Men", "Women"]
+]:
+    file_name = team_file["file_name"]
+    print("Processing " + file_name)
+    complete_results_dict[team_file["dict_key"]] = parse_team_csv(
+        team_file["file_name"], race
+    )
+
+# Write out the monster JSON file
 with open(file_prefix + ".json", "w", encoding="utf-8") as json_file:
     json.dump(complete_results_dict, json_file, ensure_ascii=False, indent=4)
